ec321f4ed4dd89f02dcac808474ad5bd4390f1ce
VigneshNallasivam <vickyshanestark@gmail.com>
2022-11-04 21:30:30 +0530

    Added: UC-1 Respond Happy or Sad

---
+++ src/mood/MoodAnalyser.java
@@ -0,0 +1,11 @@
+package mood;
+
+public class MoodAnalyser 
+{
+	public String analyseMood(String message) {
+		if (message.contains("SAD"))
+			return "SAD";
+		else
+			return "HAPPY";
+	}
+}
---
+++ src/mood/MoodAnalyserTest.java
@@ -0,0 +1,23 @@
+package mood;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+class MoodAnalyserTest {
+	@Test
+    public void testMoodAnalyse() {
+        MoodAnalyser md = new MoodAnalyser();
+        String mood = md.analyseMood("This is a SAD message");
+        Assert.assertEquals("SAD", mood);
+    }
+
+    @Test
+    public void testHappyMood() {
+        MoodAnalyser md = new MoodAnalyser();
+        String mood = md.analyseMood("This is a HAPPY message");
+        Assert.assertEquals("HAPPY", mood);
+    }
+	
+}
