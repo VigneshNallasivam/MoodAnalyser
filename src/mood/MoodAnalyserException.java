4de30f7049488dac45e958bc22b9e17540a1e081
VigneshNallasivam <vickyshanestark@gmail.com>
2022-11-04 22:00:43 +0530

    Added: UC-3 InformUser

---
+++ src/mood/MoodAnalyserException.java
@@ -0,0 +1,15 @@
+package mood;
+
+public class MoodAnalyserException extends Exception
+{
+	public enum ExceptionType {
+		ENTERED_NULL, ENTERED_EMPTY;
+	}
+
+	ExceptionType type;
+
+	public MoodAnalyserException(ExceptionType type, String message) {
+		super(message);
+		this.type = type;
+	}
+}
